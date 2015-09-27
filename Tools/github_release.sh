2d0c0f43fc78ab0e6e89afacc6c4e025d399748e
Ian Blenke <ian@blenke.com>
2015-09-26 21:45:52 -0400

    Adding comments, securing credentials, fixing versioning, targeting 10.8, adding signing

--- Tools/github_release.sh
+++ Tools/github_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Only deploy master branch builds
+
 if [ -z "$TRAVIS_BRANCH" ] ; then
   echo "TRAVIS_BRANCH not found. Deploy skipped"
   exit 0
@@ -10,41 +12,118 @@ if [ "$TRAVIS_BRANCH" != "master" ] ; then
   exit 0
 fi
 
-set -x
+set -xe
 
-curl -sL https://github.com/aktau/github-release/releases/download/v0.6.2/darwin-amd64-github-release.tar.bz2 | bunzip2 -cd | tar xf - --strip=3 -C /tmp/
+# Generate an archive for this project
 
-chmod 755 /tmp/github-release
+XCARCHIVE_FILE=/tmp/ace-mac.xcarchive
 
-tag="$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-$(git rev-parse --short HEAD)
+xctool -project ACE.xcodeproj \
+       -scheme VATRP \
+       -sdk macosx \
+       -configuration Debug \
+       -derivedDataPath build/derived \
+       archive \
+       -archivePath $XCARCHIVE_FILE 1>/dev/null
 
-/tmp/github-release release \
-    --user VTCSecureLLC \
-    --repo ace-mac \
-    --tag $tag \
-    --name "Travis-CI Automated $tag" \
-    --description "$(git log -1 --pretty=format:%B)" \
-    --pre-release
+# Prepare semantic versioning tag
 
-find . -name '*.app' -print | while read app; do
+SHA1=$(git rev-parse --short HEAD)
+
+echo "$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-${SHA1} > LastCommit.txt
+git log -1 --pretty=format:%B >> LastCommit.txt
+
+tag="$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-${SHA1}
+
+# Prepare other variables
+
+IFS=/ GITHUB_REPO=($TRAVIS_REPO_SLUG)
+
+PKG_FILE=/tmp/ace-mac.pkg
+
+# Release via HockeyApp if credentials are available
+
+set +x
+if [ -z "$HOCKEYAPP_TOKEN" ]; then
+  echo HOCKEYAPP_TOKEN is not defined. Neither creating installer pkg, nor deploying it to HockeyApp.
+else
   set -x
-  mkdir -p diskimage/
-  cp -a "$app" diskimage/
-  [ -d "$app".dSYM ] && cp -a "$app".dSYM diskimage/
-  config=$(basename $(dirname "$app"))
-  dmg=$(basename "$app" | sed -e 's/.app$//')
-  hdiutil create $dmg-$config-$tag.dmg -srcfolder diskimage/ -ov
-  rm -fr diskimage/
-  set +x
-done
 
-find . -name '*.dmg' -print | while read dmg; do
-  echo "Uploading $dmg github release $tag : $(ls -la $dmg)"
-  /tmp/github-release upload \
-      --user VTCSecureLLC \
-      --repo ace-mac \
+  # Generate an installer pkg from the archive
+
+  xcodebuild -exportArchive \
+             -exportFormat app \
+             -archivePath $XCARCHIVE_FILE \
+             -exportPath $PKG_FILE \
+             -exportProvisioningProfile 'com.vtcsecure.ace.mac development'
+
+  # Create a dSYM zip file from the archive build
+
+  DSYM_DIR=$(find build/derived -name '*.dSYM' | head -1)
+  DSYM_ZIP_FILE=${PKG_FILE}.dsym.zip
+  (cd $(dirname $DSYM_DIR) ; zip -r $DSYM_ZIP_FILE $(basename $DSYM_DIR) )
+
+  # Distribute via HockeyApp
+
+  bundle exec ipa distribute:hockeyapp \
+             --token $HOCKEYAPP_TOKEN \
+             --file $PKG_FILE \
+             --dsym $DSYM_ZIP_FILE \
+             --notes LastCommit.txt \
+             --notify \
+             --commit-sha ${SHA1} \
+             --build-server-url "https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}" \
+             --repository-url "https://github.com/${TRAVIS_REPO_SLUG}"
+fi
+
+# Create a GitHub release if credentials are available
+
+set +x
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo GITHUB_TOKEN is not defined. Neither creating dmg packages, nor creating a GitHub release.
+else
+  set -x
+  curl -sL https://github.com/aktau/github-release/releases/download/v0.6.2/darwin-amd64-github-release.tar.bz2 | bunzip2 -cd | tar xf - --strip=3 -C /tmp/
+
+  chmod 755 /tmp/github-release
+
+  /tmp/github-release release \
+      --user ${GITHUB_REPO[0]:-VTCSecureLLC} \
+      --repo ${GITHUB_REPO[1]:-ace-mac} \
       --tag $tag \
-      --name $(basename "$dmg") \
-      --file "$dmg"
-done
+      --name "Travis-CI Automated $tag" \
+      --description "$(git log -1 --pretty=format:%B)" \
+      --pre-release
 
+  find . -name '*.app' -print | grep -v build/derived | while read app; do
+    set -x
+    mkdir -p diskimage/
+    cp -a "$app" diskimage/
+    [ -d "$app".dSYM ] && cp -a "$app".dSYM diskimage/
+    config=$(basename $(dirname "$app"))
+    dmg=$(basename "$app" | sed -e 's/.app$//')
+    hdiutil create $dmg-$config-$tag.dmg -srcfolder diskimage/ -ov
+    rm -fr diskimage/
+    set +x
+  done
+
+  find . -name '*.dmg' -print | while read dmg; do
+    echo "Uploading $dmg github release $tag : $(ls -la $dmg)"
+    /tmp/github-release upload \
+        --user ${GITHUB_REPO[0]:-VTCSecureLLC} \
+        --repo ${GITHUB_REPO[1]:-ace-mac} \
+        --tag $tag \
+        --name $(basename "$dmg") \
+        --file "$dmg"
+  done
+
+  if [ -f $PKG_FILE ]; then
+    echo "Uploading $PKG_FILE github release $tag : $(ls -la $PKG_FILE)"
+    /tmp/github-release upload \
+        --user ${GITHUB_REPO[0]:-VTCSecureLLC} \
+        --repo ${GITHUB_REPO[1]:-ace-mac} \
+        --tag $tag \
+        --name $(basename "$PKG_FILE") \
+        --file "$PKG_FILE"
+  fi
+fi

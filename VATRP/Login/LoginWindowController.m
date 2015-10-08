327ac8aa869ed8634de63f13aa8296f45ff84757
Zack Matthews <zack@zackmatthews.com>
2015-10-08 16:51:31 -0400

    Fix for auto login

--- VATRP/Login/LoginWindowController.m
+++ VATRP/Login/LoginWindowController.m
@@ -23,18 +23,30 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-
     [AppDelegate sharedInstance].loginWindowController = self;
+    AccountModel *accountModel = NULL;//[[AccountsService sharedInstance] getDefaultAccount];
 
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-    
-    serviceSelectionViewController = [[NSStoryboard storyboardWithName:@"Main" bundle:nil] instantiateControllerWithIdentifier:@"ServiceSelectionViewController"];
-    
-    // Init navigation controller and add to window
-    navigationController = [[BFNavigationController alloc] initWithFrame:NSMakeRect(0, 0, self.window.frame.size.width, self.window.frame.size.height)
-                                                      rootViewController:serviceSelectionViewController];
-    
-    [self.window.contentView addSubview:navigationController.view];
+    if (accountModel &&
+        accountModel.username &&
+        accountModel.password &&
+        accountModel.domain &&
+        accountModel.transport &&
+        accountModel.port) {
+        
+        [[RegistrationService sharedInstance] asyncRegisterWithAccountModel:accountModel];
+
+        [[AppDelegate sharedInstance] performSelector:@selector(showTabWindow) withObject:nil afterDelay:0.001];
+//        [[AppDelegate sharedInstance] showTabWindow];
+    } else {
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        serviceSelectionViewController = [[NSStoryboard storyboardWithName:@"Main" bundle:nil] instantiateControllerWithIdentifier:@"ServiceSelectionViewController"];
+        
+        // Init navigation controller and add to window
+        navigationController = [[BFNavigationController alloc] initWithFrame:NSMakeRect(0, 0, self.window.frame.size.width, self.window.frame.size.height)
+                                                          rootViewController:serviceSelectionViewController];
+        
+        [self.window.contentView addSubview:navigationController.view];
 }
 
 @end

57a81dbeeb65b722843f9c7d29d1fcc0a200664e
Zack Matthews <zack@zackmatthews.com>
2015-10-12 10:14:50 -0400

    Added logic to hangup call when X is clicked on remote video window. Jira Ticket: VATRP-239

--- VATRP/VideoCall/VideoCallViewController.m
+++ VATRP/VideoCall/VideoCallViewController.m
@@ -7,7 +7,10 @@
 //
 
 #import "VideoCallViewController.h"
-
+#import "AppDelegate.h"
+#import "CallWindowController.h"
+#import "CallViewController.h"
+#import "SelfVideoViewController.h"
 @interface VideoCallViewController ()
 
 @end
@@ -16,7 +19,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowWillClose:) name:NSWindowWillCloseNotification object:self.view.window];
     // Do view setup here.
 }
+
+- (void)windowWillClose:(NSNotification *)notification
+{
+    NSWindow *win = [notification object];
+    CallWindowController *callWindowController = [AppDelegate sharedInstance].callWindowController;
+    CallViewController *callViewController;
+    
+    if(callWindowController){
+        callViewController = [callWindowController getCallViewController];
+        
+        if(callViewController){
+            if(callViewController.call){
+                LinphoneCore *lc = [LinphoneManager getLc];
+                LinphoneCall *currentCall = linphone_core_get_current_call(lc);
+                
+                if(currentCall){
+                    linphone_core_terminate_call(lc, callViewController.call);
+                }
+            }
+        }
+    }
+    
+
+}
+
+
 
 @end

e941f8db68a849702ec04bc5d77f7b0c72492741
Zack Matthews <zack@zackmatthews.com>
2015-10-19 09:43:40 -0400

    Added version number from Plist to main application window.

--- VATRP/Home/HomeWindowController.m
+++ VATRP/Home/HomeWindowController.m
@@ -25,8 +25,16 @@
     pos.x = [[NSScreen mainScreen] frame].origin.x + [[NSScreen mainScreen] frame].size.width / 2 - [self.window frame].size.width /2;
     pos.y = [[NSScreen mainScreen] frame].origin.y + ([[NSScreen mainScreen] frame].size.height / 2) + ([self.window frame].size.height * 2);
     [self setWindowPos: pos];
+
+    self.window.title = [self getACEBuildNumber];
 }
 
+-(NSString*)getACEBuildNumber{
+    NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
+    NSString* version = [infoDict objectForKey:@"CFBundleShortVersionString"];
+    
+    return [NSString stringWithFormat:@"ACE: v%@", version];
+}
 
 -(void) setWindowPos:(NSPoint) pos{
     [self.window setFrameOrigin : pos];

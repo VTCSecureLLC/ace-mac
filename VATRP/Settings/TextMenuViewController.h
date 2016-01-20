7b804a6df7b51e88d07fe0c0113ad699fb3d4087
Patrick <patrickmwatson@gmail.com>
2016-01-20 09:54:35 -0500

    Added new text settings menu option. And added the internal mechanism for handling SIP SIMPLE and RTT in call.

---
+++ VATRP/Settings/TextMenuViewController.h
@@ -0,0 +1,15 @@
+//
+//  textMenuViewController.h
+//  ACE
+//
+//  Created by Norayr Harutyunyan on 1/11/16.
+//  Copyright © 2016 VTCSecure. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface textMenuViewController : NSViewController
+
+- (void) save;
+
+@end

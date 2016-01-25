c707172f50d54f392696de6375fdb61cc253564e
Ruben Semerjyan <semrubo@gmail.com>
2016-01-25 19:24:26 +0400

    Added export all contacts (in VCARD standard form) into a file functionality.

--- VATRP/Services/ContactsService.m
+++ VATRP/Services/ContactsService.m
@@ -9,6 +9,8 @@
 #import "ContactsService.h"
 #import <AddressBook/ABPerson.h>
 #import <AddressBook/ABAddressBook.h>
+#import "LinphoneContactService.h"
+
 @implementation ContactsService
 
 -(id) init{
@@ -20,6 +22,9 @@
     NSData *vCardData = [[NSData alloc] initWithContentsOfFile:path];
     ABPerson *person = [[ABPerson alloc] initWithVCardRepresentation:vCardData];
     
+    
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:path];
+    
     NSAlert *alert = [[NSAlert alloc] init];
     
     [alert addButtonWithTitle:@"OK"];
@@ -28,23 +33,39 @@
     [alert runModal];
     return person;
 }
-+(void) exportContact: (NSString*) firstName : (NSString*) lastName : (NSString*) sipAddress :(NSString*)path{
 
-    ABPerson *person = [[ABPerson alloc] initWithAddressBook:[ABAddressBook sharedAddressBook]];
-    [person setValue:firstName forKey:kABFirstNameProperty];
-    [person setValue:lastName forKey:kABLastNameProperty];
-    [person setValue:sipAddress forKey:kABJobTitleProperty];
-    [[ABAddressBook sharedAddressBook] addRecord:(ABPerson*)person];
++(void) exportContact: (NSString*) firstName : (NSString*) lastName : (NSString*) sipAddress :(NSString*)path{
     
-    NSData *data = [person vCardRepresentation];
+    [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
+    NSMutableArray *contactInfos;
+    contactInfos = [[LinphoneContactService sharedInstance] contactList];
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:path];
+
+    for (NSDictionary *contact in contactInfos) {
+        
+        ABPerson *person = [[ABPerson alloc] initWithAddressBook:[ABAddressBook sharedAddressBook]];
+        [person setValue:[contact objectForKey:@"name"] forKey:kABFirstNameProperty];
+        [person setValue:[contact objectForKey:@"phone"] forKey:kABJobTitleProperty];
+        [[ABAddressBook sharedAddressBook] addRecord:(ABPerson*)person];
+        
+        NSData *data = [NSData new];
+        data = [person vCardRepresentation];
+        
+        if (fileHandle) {
+            [fileHandle seekToEndOfFile];
+            [fileHandle writeData:data];
+        } else {
+            NSLog(@"Error writing contacts in file");
+        }
+        
+    }
+    [fileHandle closeFile];
     
-    NSString *filePath = path;
-    [[NSFileManager defaultManager] createFileAtPath:filePath contents:data attributes:nil];
     
     NSAlert *alert = [[NSAlert alloc] init];
     
     [alert addButtonWithTitle:@"OK"];
-    [alert setMessageText:[NSString stringWithFormat:@"Contact exported to %@", filePath]];
+    [alert setMessageText:[NSString stringWithFormat:@"Contact exported to %@", path]];
     [alert setAlertStyle:NSWarningAlertStyle];
     [alert runModal];
     

7bd83fd0393e9d5e672d0eadb4b52fb045c78c1f
Ian Blenke <ian@blenke.com>
2016-02-14 03:51:38 -0500

    Factor the .travis.yml into the build and prepare scripts, so that devs can run the prepare script locally

---
+++ Tools/prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -n "$TRAVIS" ]; then
+  export HOMEBREW_PREFIX=$HOME/.homebrew
+  if [[ -d $HOMEBREW_PREFIX ]]; then echo "HOMEBREW_PREFIX=$HOMEBREW_PREFIX"; else rsync -aq /usr/local/ $HOMEBREW_PREFIX; fi
+  export PATH=$HOMEBREW_PREFIX/bin:$PATH; hash -r
+fi
+which ccache || brew install ccache || brew link --force ccache || true
+which md5deep || brew install ccache || brew link --force md5deep || true
+export CCACHE_DIR=$HOME/.ccache
+export LINPHONE_CCACHE=ccache
+export CCACHE_SLOPPINESS=pch_defines,time_macros,include_file_mtime,include_file_ctime,file_macro
+export CCACHE_COMPILERCHECK=content
+ccache -M 5G
+ccache -s 
+brew update 1>/dev/null
+brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake gettext
+brew link --force gettext
+brew install cairo --without-x11
+brew install gtk+ --without-x11
+brew install gtk-mac-integration hicolor-icon-theme
+wget --no-check-certificate https://raw.github.com/yuvi/gas-preprocessor/master/gas-preprocessor.pl
+chmod +x gas-preprocessor.pl
+[ -x /usr/local/bin/gas-preprocessor.pl ] || sudo mv -f gas-preprocessor.pl /usr/local/bin
+[ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
+git submodule update --init --recursive
+bundle install

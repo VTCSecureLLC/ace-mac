411e8300bfb333697d42a79184d9983fd6c78313
Zack Matthews <zack@zackmatthews.com>
2016-02-29 10:15:43 -0500

    Force custom video preset to avoid mediastreamer2 codec limits

--- VATRP/Services/SystemInfo.m
+++ VATRP/Services/SystemInfo.m
@@ -154,7 +154,7 @@ static inline NSString* NSStringFromBOOL(BOOL aBool) {
         /**Video**/
         NSString *videoHeader = @"\nVIDEO: \n";
         [values addObject:videoHeader];
-        linphone_core_set_video_preset(lc, "high-fps");
+        linphone_core_set_video_preset(lc, "custom");
         NSString *video_preset = [NSString stringWithFormat:@"Video preset = %s", linphone_core_get_video_preset(lc)];
         [values addObject:video_preset];
         NSString *adaptiveRateAlgorithm = [NSString stringWithFormat:@"Adaptive rate algorithm = %s",         linphone_core_get_adaptive_rate_algorithm(lc)];

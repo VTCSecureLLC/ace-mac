9b9a97b344d95094222b4202e01cc914615d9f6f
Shareef Ali <shareefalis@gmail.com>
2016-05-02 15:53:15 -0400

    anew image with location changes

--- Tools/build_dmg.sh
+++ Tools/build_dmg.sh
@@ -99,13 +99,13 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 820, 440}
+           set the bounds of container window to {400, 100, 780, 440}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
            set icon size of viewOptions to 72
            set background picture of viewOptions to file ".background:'${DMG_BACKGROUND_IMG}'"
            set position of item "'${APP_NAME}'.app" of container window to {50, 250}
-           set position of item "Applications" of container window to {360, 250}
+           set position of item "Applications" of container window to {330, 250}
            close
            open
            update without registering applications

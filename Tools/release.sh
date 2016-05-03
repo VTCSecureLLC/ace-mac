e28a22577aa27cbc23e49175abe79f2769be8448
Shareef Ali <shareefalis@gmail.com>
2016-05-02 20:34:45 -0400

    fix json script

--- Tools/release.sh
+++ Tools/release.sh
@@ -194,7 +194,7 @@ else
     echo ' -H "X-HockeyAppToken: REDACTED" \'
     echo ' https://rink.hockeyapp.net/api/2/apps/'"${HOCKEYAPP_APP_ID}"'/app_versions/upload'
 
-   json=curl \
+   curl \
      -F "status=2" \
      -F "notify=1" \
      -F "commit_sha=${SHA1}" \
@@ -210,11 +210,12 @@ else
      -F "ipa=@$APP_DMG_FILE" \
      -F "dsym=@$DSYM_ZIP_FILE" \
      -H "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
-     https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_ID}/app_versions/new || true
+     https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_ID}/app_versions/new  > json.txt
+    json=$(echo json.txt)
     prop='id'
     id='jsonval'
-    echo '$id'
-    echo '$json'
+    echo $id
+    echo $json
     curl \
       -X PUT
       -F "status=2" \

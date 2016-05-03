35524f18ebc6d9ce34ebb4cebe1ebae0b8a1d0da
Shareef Ali <shareef.ali@vtcsecure.com>
2016-05-02 22:54:06 -0400

    curl madness!

--- Tools/release.sh
+++ Tools/release.sh
@@ -194,8 +194,10 @@ else
     echo ' -H "X-HockeyAppToken: REDACTED" \'
     echo ' https://rink.hockeyapp.net/api/2/apps/'"${HOCKEYAPP_APP_ID}"'/app_versions/upload'
 
-   curl \
-     https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_ID}/app_versions/new \
+   curl  \
+     -X GET \
+     -H "Content-type: application/json" \
+     -H "Accept: application/json"  \
      -F "status=2" \
      -F "notify=1" \
      -F "commit_sha=${SHA1}" \
@@ -211,6 +213,7 @@ else
      -F "ipa=@$APP_DMG_FILE" \
      -F "dsym=@$DSYM_ZIP_FILE" \
      -H "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
+     https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_ID}/app_versions/new \
      > json.txt
     json='cat json.txt'
     prop='id'
@@ -218,7 +221,7 @@ else
     echo $id
     echo $json
     curl \
-      -X PUT
+      -X PUT \
       -F "status=2" \
       -F "notify=1" \
       -F "commit_sha=${SHA1}" \
